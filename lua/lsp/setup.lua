3b84045b2255c7d2195ae136302012bbec14f33d
jyf111 <1340549313@qq.com>
2022-02-09 23:14:52 +0800

    fix indent problem

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -4,10 +4,10 @@ local lsp_installer = require "nvim-lsp-installer"
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 -- { key: 语言 value: 配置文件 }
 local servers = {
-  sumneko_lua = require "lsp.lua", -- /lua/lsp/lua.lua
-  pyright = {},
-  clangd = {},
-  cmake = {},
+  -- sumneko_lua = require "lsp.lua", -- /lua/lsp/lua.lua
+  -- pyright = {},
+  -- clangd = {},
+  -- cmake = {},
 }
 
 -- 自动安装 LanguageServers
